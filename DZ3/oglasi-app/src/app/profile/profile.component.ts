4195b1d9bfde3079759156ace116d6cb40e18531
HrcoB <101458335+HrcoB@users.noreply.github.com>
2025-05-17 10:18:33 +0200

    fixed city error display, added search in my ads

--- DZ3/oglasi-app/src/app/profile/profile.component.ts
+++ DZ3/oglasi-app/src/app/profile/profile.component.ts
@@ -6,7 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { adInfo } from '../models/ad-classes';
 import { AdService } from '../../services/ad-creation.service';
 import { Router } from '@angular/router';
-
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-profile',
   standalone: true,
@@ -14,7 +14,8 @@ import { Router } from '@angular/router';
     MatTabsModule,
     MatIconModule,
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
@@ -23,6 +24,7 @@ export class ProfileComponent implements OnInit {
 
   userId : number = 1;
   myAds : adInfo[] = [];
+  filteredAds : adInfo[] = [];
 
   constructor(private adService : AdService, private router: Router) { }
 
@@ -30,6 +32,7 @@ export class ProfileComponent implements OnInit {
     this.adService.getAdsByUser(this.userId).subscribe({
       next: (ads : adInfo[]) => {
         this.myAds = ads;
+        this.filteredAds = ads;
       }
     })
 
@@ -37,5 +40,13 @@ export class ProfileComponent implements OnInit {
 
   editAd(adId : number){
     this.router.navigate(['/editAd', adId]);
+  }
+
+  filterAds(searchQuery : string){
+    const lowerQuery = searchQuery.toLowerCase().trim();
+    this.filteredAds = this.myAds.filter(ad =>
+      ad.title.toLowerCase().includes(lowerQuery) ||
+      ad.shortDesc.toLowerCase().includes(lowerQuery)
+    );
   }
 }
